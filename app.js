6974a6bf23d8b9a155a06f213fa69aa764d48fe4
Prabhakaran <rprabhakaran03@gmail.com>
2024-01-26 15:55:44 +0530

    Changes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const cors  =require("cors");
+
 // express
 const express = require("express");
 const app = express();
@@ -15,6 +17,7 @@ const connectDB = require("./db/connect");
 // Routes
 const userRouter = require("./routes/UserRoute");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
